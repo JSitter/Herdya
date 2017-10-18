47dfa357219c394da687440bdbeb080db6e8859a
Justin Sitter <justin@jaytria.com>
2017-10-18 08:41:17 -0700

    Wrote write_metadata method

--- logger.py
+++ logger.py
@@ -53,11 +53,11 @@ class Logger(object):
     def __init__(self, file_name):
         # TODO:  Finish this initialization method.  The file_name passed should be the
         # full file name of the file that the logs will be written to.
-        self.file_name = None
+        self.file_name = file_name
 
     def write_metadata(self, pop_size, vacc_percentage, virus_name, mortality_rate,
                        basic_repro_num):
-        # TODO: Finish this method.  The simulation class should use this method
+        # TODOj: Finish this method.  The simulation class should use this method
         # immediately upon creation, to log the specific parameters of the simulation
         # as the first line of the file.  This line of metadata should be tab-delimited
         # (each item separated by a '\t' character).
@@ -67,7 +67,10 @@ class Logger(object):
         # since 'w' overwrites the file.
         # NOTE: Make sure to end every line with a '/n' character to ensure that each
         # event logged ends up on a separate line!
-        pass
+        file = open('./logs/'+self.file_name, "w")
+        metadata = ("{}\t{}\t{}\t{}\t{}".format(pop_size, vacc_percentage, virus_name, mortality_rate, basic_repro_num))
+        file.write(metadata)
+        file.close()
 
     def log_interaction(self, person1, person2, did_infect=None,
                         person2_vacc=None, person2_sick=None):
--- test_logger.py
+++ test_logger.py
@@ -1,15 +1,18 @@
 import pytest
-import logger
+from logger import Logger
 
 def test_write_metadata():
     '''should create file with metadata'''
-    file = open("./logs/logging", 'w')
-    file.write("Hellow Worldz")
-    file.close()
+    # file = open("./logs/logging", 'w')
+    # file.write("Hellow Worldz")
+    # file.close()
+    log = Logger("test.log")
+    log.write_metadata("100", ".5", "Ebola", ".5", ".5")
 
-    file = open("./logs/logging", 'r')
+    file = open("./logs/test.log", 'r')
     line_one = file.readline()
     file.close()
-    assert line_one == "Hellow Worldz"
+    assert line_one == "100\t.5\tEbola\t.5\t.5"
+    
 def test_log_interaction():
     pass
