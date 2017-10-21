d108c9bc6a6d12fa15e2d1f084c421357428cecd
Justin Sitter <justin@jaytria.com>
2017-10-21 13:28:39 -0700

    add test_sim_pop

--- test_sim.py
+++ test_sim.py
@@ -11,3 +11,6 @@ def test_sim():
     os.system('python3 ./simulation.py 1000 .2 cat .5 1.2')
 
 
+def test_sim_pop():
+    sim = Simulation(10000, .2,"cat", .5, 1.2, 1)
+    assert len(sim.population) == 10000
