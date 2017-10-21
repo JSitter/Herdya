9a6e7c53568b19cb0f8a79ec10601fa8e96f8358
Justin Sitter <justin@jaytria.com>
2017-10-21 12:15:55 -0700

    added tests

--- test_person.py
+++ test_person.py
@@ -5,10 +5,10 @@ from virus import Virus
 def test_did_survive_infection():
     avg = 0
     vir = Virus("cat", .5, 12)
-    for i in range(1000):
+    for i in range(10000):
         pat = Person("pat", False, vir)
         pat.did_survive_infection()
         if pat.is_alive:
             avg += 1
-    assert avg > 400 and avg < 600
+    assert avg > 4900 and avg < 5100
 
--- test_sim.py
+++ test_sim.py
@@ -1,4 +1,11 @@
 import os
+from simulation import Simulation
+
+def test_simulation_instance():
+    ''' test that simuation can be instantiated '''
+    assert Simulation(1000, .2,"cat", .5, 1.2, 1)
+
+
 
 def test_sim():
     os.system('python3 ./simulation.py 1000 .2 cat .5 1.2')
